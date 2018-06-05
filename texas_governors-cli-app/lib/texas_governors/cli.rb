2c688675188eb626bf75c921ec901163d16aa8f3
Wasif Khan <waasif.khaan@gmail.com>
2018-06-05 21:25:23 +0000

    Done.

---
+++ texas_governors-cli-app/lib/texas_governors/cli.rb
@@ -0,0 +1,14 @@
+class TexasGovernors::CLI 
+  def call 
+  
+    start 
+  
+  end 
+  
+  def start 
+  
+  end 
+
+
+
+end 
---
+++ texas_governors-cli-app/lib/texas_governors/governor.rb
@@ -0,0 +1,14 @@
+class TexasGovernors::Governor
+  attr_accessor :name, :profile_url, :born_death_date
+  
+  
+  def initialize 
+    @name 
+    @profile_url
+    @born_death_date
+    @
+    
+
+
+
+end 
