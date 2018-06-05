01165fb812b10f9a507da4b5591dc1cc6976e0f3
Wasif Khan <waasif.khaan@gmail.com>
2018-06-05 21:54:01 +0000

    Done.

--- texas_governors-cli-app/lib/texas_governors/cli.rb
+++ texas_governors-cli-app/lib/texas_governors/cli.rb
@@ -6,6 +6,10 @@ class TexasGovernors::CLI
   end 
   
   def start 
+    puts "Welcome to the app for the chronological history of Governors that served the state of Texas since 1876 "
+    puts "Here is the list of Governors "
+    list_available_deals
+    details_menu
   
   end 
 
--- texas_governors-cli-app/lib/texas_governors/scraper.rb
+++ texas_governors-cli-app/lib/texas_governors/scraper.rb
@@ -1,4 +1,5 @@
 class TexasGovernors::Scraper
+  attr_accessor :doc, :name, :profile_url, :born_death_date, :age, :term_in_office, :party_affiliation, :elected_year
   
   def get_page
     
@@ -13,12 +14,13 @@ class TexasGovernors::Scraper
         scraped_governor = {}
         small_list = tr_list.search("small")
         scraped_governor[:name] = tr_list.css("td big").first.text
+        scraped_governor[:age] = small_list[1].text.gsub(/[()]/, "")
         scraped_governor[:profile_url] = "https://en.wikipedia.org#{tr_list.css("td big b a").attribute("href").value}"
         scraped_governor[:born_death_date] = tr_list.css("td")[3].css("small").first.text  
-        scraped_governor[:age] = small_list[1].text.gsub(/[()]/, "")
+        scraped_governor[:elected_year] = tr_list.css("td")[6].text
         scraped_governor[:term_in_office] = "#{tr_list.css("td")[4].children[0].text} - #{tr_list.css("td")[4].children[0].text.gsub(/[\n]/, '')}"
         scraped_governor[:party_affiliation] = tr_list.css("td")[5].text
-        scraped_governor[:elected_year] = tr_list.css("td")[6].text
+        
         array_governors << scraped_governor 
     end
     array_governors
@@ -26,6 +28,8 @@ class TexasGovernors::Scraper
   end 
   
   # OR we can instantiate the objects based on the governor class
+   
+   
 
 
 
